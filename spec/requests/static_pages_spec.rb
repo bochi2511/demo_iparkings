1aa47e0a1d43fa18b7cab529ae79d072cca171b0
bochi <bochi2511@gmail.com>
2013-10-06 13:02:12 +0200

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,9 @@ describe "Static Pages" do
   		expect(page).to have_content('iParkings Demo')
   	end
 
-  	it "should have the title 'Home'" do
+  	it "should have the base title" do
   		visit '/static_pages/home'
-  		expect(page).to have_title("iParkings Demo App | Home")  			
+  		expect(page).to have_title("iParkings Demo App")  			
   	end  		
   end
 end
